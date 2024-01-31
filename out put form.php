3986556c9eab877e980dabb4b14cdf72ef6791a7
aimanahmadali480 <158254012+aimanahmadali480@users.noreply.github.com>
2024-01-31 13:15:33 +0700

    Add files via upload

---
+++ out put form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width-device-width, initial-scale-1.0">
+    <title>dokumen</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td><?php echo $_POST['nama']?></td>
+        </tr>
+    </table>
+</body>
+</html>
